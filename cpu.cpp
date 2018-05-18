129b739386fbb40042612736ecf03aaef97bc5f1
donfreiday <donfreiday@gmail.com>
2018-05-18 13:59:42 -0400

    Implement CPU timings required to emulate LCD

--- cpu.cpp
+++ cpu.cpp
@@ -44,6 +44,8 @@ bool CPU::execute(u8 op) {
     // NOP
     case 0x00:
     printf("NOP\n");
+    cpu_clock_m = 1;
+    cpu_clock_t = 4;
     cycles += 4;
     break;
 
@@ -51,7 +53,9 @@ bool CPU::execute(u8 op) {
     case 0x05:
     printf("DEC b\n");
     decrement_reg(reg.b);
-    cycles+=4;
+    cpu_clock_m = 1;
+    cpu_clock_t = 4;
+    cycles += 4;
     break;
 
     //LD b n
@@ -59,6 +63,8 @@ bool CPU::execute(u8 op) {
     printf("LD b %02X\n", mmu.read_u8(reg.pc));
     reg.b = mmu.read_u8(reg.pc);
     reg.pc += 1;
+    cpu_clock_m = 2;
+    cpu_clock_t = 8;
     cycles += 8;
     break;
 
@@ -66,6 +72,8 @@ bool CPU::execute(u8 op) {
     case 0x0D:
     printf("DEC c\n");
     decrement_reg(reg.c);
+    cpu_clock_m = 1;
+    cpu_clock_t = 4;
     cycles+=4;
     break;
 
@@ -74,6 +82,8 @@ bool CPU::execute(u8 op) {
     printf("LD c %02X\n", mmu.read_u8(reg.pc));
     reg.c = mmu.read_u8(reg.pc);
     reg.pc += 1;
+    cpu_clock_m = 2;
+    cpu_clock_t = 8;
     cycles += 8;
     break;
 
@@ -84,9 +94,13 @@ bool CPU::execute(u8 op) {
     printf("JR nz %02X\n", mmu.read_u8(reg.pc));
     if (!(reg.f & 0x80)) {
       reg.pc+=(s8)(mmu.read_u8(reg.pc));
+      cpu_clock_m = 3;
+      cpu_clock_t = 12;
       cycles+=12;
     }
     else {
+      cpu_clock_m = 2;
+      cpu_clock_t = 8;
       cycles+=8;
     }
     reg.pc++;
@@ -97,6 +111,8 @@ bool CPU::execute(u8 op) {
     printf("LD l %02X\n", mmu.read_u8(reg.pc));
     reg.l = mmu.read_u8(reg.pc);
     reg.pc += 1;
+    cpu_clock_m = 2;
+    cpu_clock_t = 8;
     cycles += 8;
     break;
 
@@ -105,6 +121,8 @@ bool CPU::execute(u8 op) {
     printf("LD hl %04X\n", mmu.read_u16(reg.pc));
     reg.hl = mmu.read_u16(reg.pc);
     reg.pc += 2;
+    cpu_clock_m = 3;
+    cpu_clock_t = 12;
     cycles += 12;
     break;
 
@@ -113,6 +131,8 @@ bool CPU::execute(u8 op) {
     case 0x32:
     printf("LDD (hl) a\n");
     mmu.write_u8(reg.hl--, reg.a);
+    cpu_clock_m = 2;
+    cpu_clock_t = 8;
     cycles += 8;
     break;
 
@@ -120,6 +140,8 @@ bool CPU::execute(u8 op) {
     case 0x3E:
     printf("LD a %02X\n", mmu.read_u8(reg.pc));
     reg.a = mmu.read_u8(reg.pc++);
+    cpu_clock_m = 2;
+    cpu_clock_t = 8;
     cycles+=8;
     break;
 
@@ -134,13 +156,17 @@ bool CPU::execute(u8 op) {
     if(reg.a==0) {
       reg.f |= 0x80;
     }
-    cycles+=4;
+    cpu_clock_m = 1;
+    cpu_clock_t = 4;
+    cycles += 4;
     break;
 
     // JP nn
     case 0xC3:
     printf("JP %04X\n", mmu.read_u16(reg.pc));
     reg.pc = mmu.read_u16(reg.pc);
+    cpu_clock_m = 4;
+    cpu_clock_t = 16;
     cycles += 16;
     break;
 
@@ -149,7 +175,9 @@ bool CPU::execute(u8 op) {
     case 0xE0:
     printf("LDH %04X a\n",0xFF00+mmu.read_u8(reg.pc));
     mmu.write_u8(0xFF00+mmu.read_u8(reg.pc), reg.a);
-    cycles+=12;
+    cpu_clock_m = 3;
+    cpu_clock_t = 12;
+    cycles += 12;
     break;
 
     // LDH a n
@@ -157,7 +185,9 @@ bool CPU::execute(u8 op) {
     case 0xF0:
     printf("LDH %04X a\n", mmu.read_u8(0xFF00+mmu.read_u8(reg.pc)));
     reg.a = mmu.read_u8(mmu.read_u8(0xFF00+mmu.read_u8(reg.pc++)));
-    cycles+=12;
+    cpu_clock_m = 3;
+    cpu_clock_t = 12;
+    cycles += 12;
     break;
 
     // DI
@@ -165,7 +195,9 @@ bool CPU::execute(u8 op) {
     case 0xF3:
     printf("DI\n");
     interrupt = false;
-    cycles+=4;
+    cpu_clock_m = 3;
+    cpu_clock_t = 12;
+    cycles += 4;
     break;
 
     default:
