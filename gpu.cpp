62e3b5116c8adc7cd7e93d9b075b6f3723d7a3e3
Don <donfreiday@gmail.com>
2018-10-04 12:55:52 -0400

    GPU: use WebGL 1.0 compatible texture filtering

--- gpu.cpp
+++ gpu.cpp
@@ -410,12 +410,14 @@ void GPU::requestInterrupt(u8 interrupt) {
 }
 
 void GPU::renderScreen() {
-    // Create OpenGL texture
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D,texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, screenData);
-    
-    vsync = true; // Flag for main loop
+  // Create OpenGL texture
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+               GL_UNSIGNED_BYTE, screenData);
+
+  vsync = true;  // Flag for main loop
 }
